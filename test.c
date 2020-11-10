3e3c798b57df853e40e5d3e558d1ed31df662bb2
charfweh <affan.ismail2016@gmail.com>
2020-11-10 20:55:15 +0530

    wtf

--- test.c
+++ test.c
@@ -3,5 +3,6 @@ int main()
 {
    printf("Hello, World!");
    printf("goodamn its been so long since i did C huh\nidek what im doing with this");
+   printf("I dont get this git branch wtf is happening0");
    return 0;
 }
