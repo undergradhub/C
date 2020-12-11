5e119eb16835c7fa4ede20f13400b747c9529061
charfweh <affan.ismail2016@gmail.com>
2020-12-11 19:44:06 +0530

    Doubly Linked List

---
+++ Linked List/doubly_ll.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct doubly{
+	int data;
+	struct doubly *next;
+	struct doubly *prev;
+};
+struct doubly *head;
+
+// ADD AT BEGINNING
+void add_at_start(int data){
+	struct doubly *newnode = malloc(sizeof(struct doubly));
+	newnode->data = data;
+	if(head == NULL){
+		newnode->prev = NULL;
+		newnode->next = NULL;
+		head = newnode;
+	}else{
+		newnode->next = head;
+		head->prev = newnode;
+		newnode->prev = NULL;
+		head = newnode;
+	}
+}
+
+// ADD AT END
+void add_at_end(int data){
+	struct doubly *newnode = malloc(sizeof(struct doubly));
+	newnode->data = data;
+	if(head == NULL){
+		add_at_start(data);
+		return;
+	}else{
+		struct doubly *ptr;
+		ptr = head;
+		while(ptr->next != NULL){
+			ptr = ptr->next;
+		}
+		newnode->next = NULL;
+		ptr->next = newnode;
+		newnode->prev = ptr;
+	}
+}
+
+// DELETE AT START
+void delete_at_start(){
+	if(head == NULL){
+		printf("[!] List is empty\n");
+		return;
+	}
+	if(head->next == NULL){
+		head = NULL;
+		free(head);
+		return;
+	}else{
+		struct doubly *temp;
+		temp = head;
+		head = head->next;
+		head->prev = NULL;
+		free(temp);
+	}
+}
+
+// DELETE AT END
+void delete_at_end(){
+	if(head == NULL){
+		printf("[!] List is empty\n");
+		return;
+	}
+	if(head->next == NULL){
+		delete_at_start();
+		return;
+	}else{
+		struct doubly *temp;
+		temp = head;
+		while(temp->next->next != NULL){
+			temp = temp->next;
+		}
+		temp->next = NULL;
+		free(temp->next);
+	}
+}
+
+// SHOW
+void show(struct doubly *ptr){
+	ptr = head;
+	if(ptr == NULL){
+		printf("[!] List is empty\n");
+		return;
+	}
+	while(ptr != NULL){
+		printf("[!] Element: %d\tPrev: %p\t Self: %p\t Next: %p\n",ptr->data, ptr->prev,ptr, ptr->next);
+		ptr = ptr->next;
+	}
+}
+
+// MAIN ENTRY
+int main(){
+	add_at_start(10);
+	add_at_start(20);
+	add_at_end(30);
+	add_at_end(40);
+	// delete_at_start();
+	delete_at_end();
+	show(head);
+}
+
+
+
+
